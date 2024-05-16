e4a0a1bbc6f3581ed17fba09e86d9635ce50ac82
Cynthia Castillo <ca.castillo@gmail.com>
2024-05-16 10:46:22 -0600

    Condicionales y Bucles

--- Fundamentos de la Web/5.- Javascript/intro.js
+++ Fundamentos de la Web/5.- Javascript/intro.js
@@ -95,7 +95,95 @@ if(edad_conducir >= 18/*f*/ || permisoPadres) { // || -> una u otra condicional 
 }
 
 /* BUCLES/CICLOS */
-for(let i=0; i < 4; i++) {
+/*
+i = 0
+Hola
+Cómo estás?
+----
+i = 1
+Hola
+Cómo estás?
+----
+i = 2
+Hola
+Cómo estás?
+----
+i = 3
+Hola
+Cómo estás?
+----
+i = 4
+*/
+let a = "B";
+
+for(let i=0; i < 4; i++) { /* for(inicializar; condicional; paso)  */
     console.log("Hola");
     console.log("¿Cómo estás?");
+    let a = "A";
+    console.log(a); // A
 }
+console.log(a); //B
+//ya no existe i
+//console.log(i); i solo existe en el bloque de código en el que la declaré
+
+for(let i=3; i > 0; i--){
+    console.log(i);
+}
+
+/*
+x = 0
+Entramos al while
+x = 2
+----
+Entramos al while
+x = 4
+----
+*/
+let x = 0;
+while(x < 3){
+    console.log("Entramos al while");
+    x += 2;
+}
+
+let inicio = 2;
+let final = 10;
+/*
+inicio = 2
+final = 10
+Entramos al segundo while
+inicio = 3
+final = 8
+---
+Entramos al segundo while
+inicio = 4
+final = 6
+---
+Entramos al segundo while
+inicio = 5
+final = 4
+---
+*/
+while(inicio < final) {
+    console.log("Entramos al segundo while");
+    inicio++;
+    final -= 2;
+}
+
+// == ===
+
+var variable1 = 1; //num
+var variable2 = "1"; //string
+
+if(variable1 == variable2) { // == -> buscando que los valores sean iguales
+    console.log("Tiene el mismo valor");
+}
+
+if(variable1 === variable2) { // === -> mismo valor y mismo tipo
+    console.log("Tienen el mismo valor Y el mismo tipo");
+}
+
+console.log(variable1+variable2);
+
+// Sigma -  Escribe código que sume todos los valores del 1 al 100 en una variable sum y, al final, console.log dé como resultado 1 + 2 + 3 + ... + 98 + 99 + 100. Deberíamos obtener 5050 al final.
+
+//Sigma pares e impares - Escribe código que sume los valores pares del 1 al 100 en una variable suma_pares y valores impares en una variable suma_impares. Intenta hacer UN solo bucle. % -> mod. 5%2 
