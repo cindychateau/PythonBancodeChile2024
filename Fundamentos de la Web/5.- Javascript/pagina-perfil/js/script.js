42331a8140d2345da2925bcbac7123522028f7c6
Cynthia Castillo <ca.castillo@gmail.com>
2024-05-20 11:05:58 -0600

    Query Selector

--- Fundamentos de la Web/5.- Javascript/pagina-perfil/js/script.js
+++ Fundamentos de la Web/5.- Javascript/pagina-perfil/js/script.js
@@ -25,3 +25,45 @@ function regresa_imagen(elemento_img) {
     //src -> la ruta de la imagen
     elemento_img.src = "images/jane-m.jpg";
 }
+
+function editar_perfil() {
+    //querySelector -> seleccionar cualquier elemento de mi documento HTML, usando selectores
+    
+    //.getElementById("identificador")
+    var elemento_nombre = document.querySelector("#nombre");
+    elemento_nombre.innerText = "Cynthia Castillo";
+
+    //.getElementByClassname("clase")
+    var lugar = document.querySelector('.lugar');
+    lugar.innerText = "Monterrey";
+    //.style -> cambia estilos
+    //.propiedadACambiar
+    lugar.style.color = "purple";
+    //.lugar { text-transform: "uppercase" }
+    lugar.style.textTransform = "uppercase";
+
+    var parrafito = document.querySelector(".card-body p"); //Primer párrafo
+    //innerHTML -> ingresar texto+etiquetas
+    parrafito.innerHTML = "Desarrolladora, Instructora de Programación <br> Mamá gatuna";
+
+    //.querySelectorAll -> Lista/array
+    /*
+    var parrafos = document.querySelectorAll('.card-body p');
+    parrafos[0] = primerparrafo;
+    parrafos[1] = segundoparrafo;
+    */
+
+}
+
+function cambiar_mode() {
+    var elemento_body = document.querySelector("body");
+    //classList -> array con las clases que tiene mi elemento
+
+    if(elemento_body.classList.contains("dark-mode")) {
+        elemento_body.classList.remove("dark-mode");
+    } else {
+        elemento_body.classList.add("dark-mode");
+    }
+
+    
+}
