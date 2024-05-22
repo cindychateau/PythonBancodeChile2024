3fad2d178b02038d1b8fa0355fa27eae65b85edf
Cynthia Castillo <ca.castillo@gmail.com>
2024-05-22 07:54:03 -0600

    Adivina Numero base

---
+++ Fundamentos de la Web/5.- Javascript/adivina-numero/script.js
@@ -0,0 +1,17 @@
+//Generar un número aleatorio
+var numeroAleatorio = Math.random(); // 0 - 1 ¿Cómo hacerlo entre 0-100?
+
+/*
+var input = document.querySelector('#numero');
+input.value;
+*/
+/*
+Comparar el numero aleatorio con el número ingresado y colocar un mensaje en la class resultado.
+NO es -> rojo
+SI es -> verde
+
+parseInt() -> texto en número entero
+*/
+function adivina() {
+
+}
