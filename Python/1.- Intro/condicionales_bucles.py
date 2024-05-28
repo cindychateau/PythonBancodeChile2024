8232e626dc9685f22c04ab3a984015bac268e20c
Cynthia Castillo <ca.castillo@gmail.com>
2024-05-28 09:19:47 -0600

    Fors

--- Python/1.- Intro/condicionales_bucles.py
+++ Python/1.- Intro/condicionales_bucles.py
@@ -40,3 +40,59 @@ if edad > 18 or permisoPadres: #si una u otra se cumple
     #Otra linea
     #Otra linea
     #Otra linea
+
+#==== BUCLE/CICLOS ====
+for x in range(5): #Rango 0-4. 5 YA NO ENTRA. x < 5
+    print(x)
+
+print("-------")
+
+for y in range(5, 13): #(Comienzo, Fin) -> y=5; y<13
+    print(y)
+
+print("-------")
+
+for z in range(2, 12, 2): #(Comienzo, Fin, Paso) -> z=2; z < 12; z+=2
+    print(z)
+
+print("-------")
+
+for m in range(30, 15, -2):
+    print(m)
+
+#RECORRER UN ARREGLO
+lista_nombres = ["Elena", "Juana", "Pablo", "Pedro"]
+for nombre in lista_nombres:
+    print(nombre)
+
+for indice in range(len(lista_nombres)): #i=0; i < 4. i = indice
+    print(f"Indice: {indice}; Valor: {lista_nombres[indice]}")
+
+estudiante = { #Equivalente a objetos en javascript
+    "nombre": "Elena", #clave: valor
+    "apellido": "De Troya",
+    "edad": 19
+}
+'''
+dato = "nombre"
+print Clave:nombre. Valor: Elena
+---
+dato = "apellido"
+print Clave: apellido. Valor: De Troya
+---
+dato = "edad"
+print Clave: edad. Valor: 19
+'''
+for dato in estudiante: #recorre claves
+    print(f"Clave: {dato}. Valor:{estudiante[dato]}")
+
+lista_estudiantes = [
+    {"nombre": "Elena", "apellido": "De Troya", "id": 123},
+    {"nombre": "Juana", "apellido": "De Arco", "id": 234},
+    {"nombre": "Pedro", "apellido": "Páramo", "id": 345}
+]
+
+for estudiante in lista_estudiantes:
+    print(estudiante)
+    for dato in estudiante:
+        print(f"Clave: {dato}. Valor:{estudiante[dato]}")
