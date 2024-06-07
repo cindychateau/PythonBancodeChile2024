5124c26412fae5eb4ca620626e68faa0aed0a7ea
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2024-06-07 13:02:36 -0500

    update

--- Python/3.- Flask/loteria_mexicana1/server.py
+++ Python/3.- Flask/loteria_mexicana1/server.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#RUTAS
+# RUTAS
+
+
 @app.route("/loteria")
 def nivel1():
     return render_template("index.html")
+
+
+@app.route("/loteria/<int:filas>")
+def nivel2(filas):
+    print(filas)
+    colores=["rosa","azul","amarillo"]
+    return render_template("loteria4xn.html", filas=filas,colores=colores)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
