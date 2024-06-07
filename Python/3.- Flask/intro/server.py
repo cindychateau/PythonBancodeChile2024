52117e11c5c0aa9df914d73034cb814fad5c8d4d
Cynthia Castillo <ca.castillo@gmail.com>
2024-06-07 09:19:15 -0600

    Plantillas y Jinja

--- Python/3.- Flask/intro/server.py
+++ Python/3.- Flask/intro/server.py
@@ -4,7 +4,7 @@ Despliegue servidor ---> python server.py o python3 server.py
 ctrl + c ---> Para detener el servidor 
 exit ---> Para salirnos del entorno virtual
 '''
-from flask import Flask #Importa Flask y me permite crear la aplicación web
+from flask import Flask, render_template #Importa Flask y me permite crear la aplicación web
 
 app = Flask(__name__) #Crea una nueva instancia de la clase Flask llamada "app"
 
@@ -19,8 +19,20 @@ def hola(name):
 @app.route("/numero/<int:n>") #Forzosamente indico que en ese espacio debe ir un entero
 def numero(n):
     print(f"El usuario ingresó {n}")
-    respuesta = f"Ingresaste el numero {n}"
+    respuesta = f"<h1>Ingresaste el numero {n}</h1>"
     return respuesta
+
+@app.route("/bienvenidas")
+def bienvenidas():
+    return render_template("index.html")
+
+# /usuarios/Pedro Paramo/5
+@app.route("/usuarios/<name>/<int:num>")
+def usuarios(name, num):
+    #name = "Pedro Paramo"
+    #num = 5
+    usuarios = ["Juana de Arco", "Pablo Picasso", "Pedro Páramo"]
+    return render_template("usuarios.html", nombre=name, numero=num, usuarios=usuarios)
 
 #Asegura que este archivo que el se está ejecutando directamente en el módulo
 if __name__ == "__main__": 
