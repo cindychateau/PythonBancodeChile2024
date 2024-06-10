f66bd9fc26eb8643c450224279152e233cdf0788
Cynthia Castillo <ca.castillo@gmail.com>
2024-06-10 10:17:53 -0600

    Loteria Mexicana terminado

--- Python/3.- Flask/loteria_mexicana1/server.py
+++ Python/3.- Flask/loteria_mexicana1/server.py
@@ -7,15 +7,21 @@ app = Flask(__name__)
 
 @app.route("/loteria")
 def nivel1():
-    return render_template("index.html")
+    return render_template("index.html", filas=4, columnas=4)
 
+# @app.route("/loteria/<int:filas>")
+# def nivel2(filas):
+#     print(filas)
+#     colores=["rosa","azul","amarillo"]
+#     return render_template("loteria4xn.html", filas=filas,colores=colores)
 
-@app.route("/loteria/<int:filas>")
-def nivel2(filas):
-    print(filas)
-    colores=["rosa","azul","amarillo"]
-    return render_template("loteria4xn.html", filas=filas,colores=colores)
+@app.route("/loteria/<int:filas>") #/loteria/10
+def nivel2(filas): #filas = 10
+    return render_template("index.html", filas=filas, columnas=4) #enviando filas=10
 
+@app.route("/loteria/<int:filas>/<int:columnas>")
+def nivel3(filas, columnas):
+    return render_template("index.html", filas=filas, columnas=columnas)
 
 if __name__ == "__main__":
     app.run(debug=True)
