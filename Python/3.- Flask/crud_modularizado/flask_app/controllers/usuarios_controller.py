17175703dce10e7907834f8d06e22f7520e8af3e
Cynthia Castillo <ca.castillo@gmail.com>
2024-06-19 08:02:09 -0600

    Crud modularizado

--- Python/3.- Flask/crud_modularizado/flask_app/controllers/usuarios_controller.py
+++ Python/3.- Flask/crud_modularizado/flask_app/controllers/usuarios_controller.py
@@ -8,3 +8,39 @@ from flask_app import app
 from flask_app.models.usuario import Usuario
 
 #RUTAS
+@app.route("/")
+def index():
+    #enviar todos los usuarios
+    usuarios = Usuario.mostrar_usuarios() #lista con todos los usuarios
+    return render_template("index.html", listado_usuarios = usuarios)
+
+@app.route("/nuevo")
+def nuevo():
+    return render_template("nuevo.html")
+
+@app.route("/crear", methods=["POST"])
+def crear():
+    #Recibimos lo que el usuario ingreso en form
+    #request.form = DICCIONARIO con la info
+    Usuario.guardar(request.form)
+    return redirect("/")
+
+@app.route("/borrar/<int:id>") #/borrar/2
+def borrar(id): #id = 2
+    diccionario = {"id": id} #{"id": 2}
+    Usuario.eliminar(diccionario)
+    return redirect("/")
+
+@app.route("/editar/<int:id>") #/editar/2
+def editar(id):
+    #Una función en Usuario que en base a un ID me regrese una instancia de Usuario
+    diccionario = {"id": id} #{"id": 2}
+    usuario = Usuario.mostrar_un_usuario(diccionario)
+    return render_template("editar.html", usuario=usuario) #enviando el objeto de Usuario a editar
+
+@app.route("/actualizar", methods=['POST'])
+def actualizar():
+    #recibimos el request.form
+    #Un método en Usuario que actulice la info de un registro enviando request.form
+    Usuario.actualizar_usuario(request.form)
+    return redirect("/")
--- Python/3.- Flask/crud_modularizado/server.py
+++ Python/3.- Flask/crud_modularizado/server.py
@@ -1,5 +1,17 @@
+#python3 o python -m pipenv install flask pymysql
+#pipenv install flask pymysql ->Instalar y generar los Pipfiles (OJO solo es necesaria 1 vez)
+#pipenv shell -> Activa entorno virtual
+#python3 server.py -> Ejecuta mi app
+
+#Ctrl + C -> Detener app
+#exit -> Salir del entorno virtual
+
 #Importación de flask_app
+from flask_app import app
 
 #Importación controladores
+from flask_app.controllers import usuarios_controller
 
 #Ejecución de la app
+if __name__ == "__main__":
+    app.run(debug=True)
