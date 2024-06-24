04eca1ff3aaecbb53132113c817403cfc49d8950
Cynthia Castillo <ca.castillo@gmail.com>
2024-06-24 10:24:49 -0600

    Registro

--- Python/3.- Flask/registro_login/flask_app/controllers/users_controller.py
+++ Python/3.- Flask/registro_login/flask_app/controllers/users_controller.py
@@ -4,9 +4,40 @@ from flask_app import app
 #Importamos los modelos
 from flask_app.models.user import User
 
-#PEND: Importar BCrypt
+#Importar BCrypt
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 #Plantilla que muestra formularios
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#Ruta que recibe el formulario
+@app.route("/register", methods=['POST'])
+def register():
+    #request.form = {"first_name": "Elena", "last_name": "De Troya"....}
+
+    #Validar la info que recibimos
+    if not User.validate_user(request.form):
+        #No es valida la info, redirecciono al form
+        return redirect("/")
+
+    #Hashear contraseña
+    pass_hash = bcrypt.generate_password_hash(request.form["password"])
+    
+    #Crear un diccionario que simule el form, incluyendo la contraseña hasheada
+    form = {
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"],
+        "password": pass_hash
+    }
+
+    id = User.save(form) #Recibo el ID del nuevo usuario 1
+    session["user_id"] = id
+    return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
--- Python/3.- Flask/registro_login/flask_app/models/user.py
+++ Python/3.- Flask/registro_login/flask_app/models/user.py
@@ -1,5 +1,10 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
+from flask import flash #flash es el encargado de mostrar los mensajes
+
+import re #Expresiones Regulares -> Empatar con un patrón de texto
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') #Expresion regular de email
+
 class User:
 
     def __init__(self, data):
@@ -33,3 +38,45 @@ class User:
             #result = [ {"id": 1, "first_name": "Elena", "last_name": "De Troya".....} ]
             user = cls(result[0]) #User({"id": 1, "first_name": "Elena", "last_name": "De Troya".....})
             return user
+    
+    #Método que valide la info que recibimos del form
+    @staticmethod
+    def validate_user(form):
+        #form = {diccionario con toda la info del formulario}
+        is_valid = True #Asumimos que el formulario si es valido
+
+        #Validamos que el nombre tenga al menos 2 caracteres
+        if len(form["first_name"]) < 2:
+            flash("First name must have at least 2 chars", "register") #(mensaje, categoria)
+            is_valid = False
+        
+        #Validamos que el apellido tenga al menos 2 caracteres
+        if len(form["last_name"]) < 2:
+            flash("Last name must have at least 2 chars", "register")
+            is_valid = False
+        
+        if len(form["password"]) < 6:
+            flash("Password must have at least 6 chars", "register")
+            is_valid = False
+        
+        #Validar que el correo sea único
+        query = "SELECT * FROM users WHERE email = %(email)s"
+        result = connectToMySQL("loginreg_bc").query_db(query, form) #Lista de diccionarios
+        #Lista vacía, Lista con 1 diccionario
+        if len(result) >= 1:
+            #SI existe ese correo en mi BD
+            flash("E-mail already registered", "register")
+            is_valid = False
+        
+        #Verificamos que las contraseñas coincidan
+        if form["password"] != form["confirm"]:
+            flash("Passwords do not match", "register")
+            is_valid = False
+        
+        #Validar que el email tenga el formato correcto
+        if not EMAIL_REGEX.match(form["email"]): #match -> compara una expresión regular con un texto
+            flash("E-mail not valid", "register")
+            is_valid = False
+        
+        return is_valid
+
